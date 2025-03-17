0138d72b2db5139ccf93cc3c51ccb064064185b3
CodeZX14 <163320692+CodeZX14@users.noreply.github.com>
2025-03-17 08:17:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 # from sklearn.svm import SVC
 # from sklearn.model_selection import train_test_split
 # from sklearn.metrics import accuracy_score
-import extract_v3
+# import extract_v3
 import categorize
 from PIL import Image
 
